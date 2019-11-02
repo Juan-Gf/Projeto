a5ffaa7d476251e5ff6be9a5f2f0d3aa10a75972
Devakian <Devakian@DESKTOP-P6SE5RJ>
2019-11-02 18:19:36 -0200

    Alteracao das arrays produto e cliente, funcionando

--- ProjetoPI2/src/view/ListaCliente.java
+++ ProjetoPI2/src/view/ListaCliente.java
@@ -202,7 +202,7 @@ public class ListaCliente extends javax.swing.JFrame {
                 int numeroLinha = tblClienteC.getSelectedRow();
                 int salvarId = Integer.parseInt(tblClienteC.getModel().getValueAt(numeroLinha,0 ).toString());
             if (tblClienteC.getSelectedRow() >= 0) {                
-                new ModifcaClienteView().setVisible(true);
+                new ModifcaClienteView(salvarId).setVisible(true);
                 this.dispose();
             } else {
                 JOptionPane.showMessageDialog(this, "Selecione um cliente para editar!");
